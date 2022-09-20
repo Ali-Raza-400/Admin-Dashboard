a09525a7aaa6fc2bc166e5cf8e400aebdab8bc77
Ali <aliraza.12748g@gmail.com>
2022-09-20 23:57:15 +0500

    chart section completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,27 @@
+import Chart from '../../component/chart/Chart'
+import Featured from '../../component/featured/Featured'
+import Navbar from '../../component/navbar/Navbar'
 import Sidebar from '../../component/sidebar/Sidebar'
+import Widgets from '../../component/widgets/Widgets'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
-      <Sidebar/>
-      <div className="homeContainer">container</div>
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earnings" />
+          <Widgets type="balance" />
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Chart/>
+        </div>
+      </div>
     </div>
   )
 }
