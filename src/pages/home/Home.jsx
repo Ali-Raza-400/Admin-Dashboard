4a7c9bbc71a9073629c713b6c7abfeeb9904df5d
Ali <aliraza.12748g@gmail.com>
2022-09-22 01:00:37 +0500

    table section completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Chart from '../../component/chart/Chart'
 import Featured from '../../component/featured/Featured'
 import Navbar from '../../component/navbar/Navbar'
 import Sidebar from '../../component/sidebar/Sidebar'
+import List from '../../component/table/Table'
+
 import Widgets from '../../component/widgets/Widgets'
 import './home.scss'
 
@@ -18,8 +20,14 @@ const Home = () => {
           <Widgets type="balance" />
         </div>
         <div className="charts">
-          <Featured/>
-          <Chart/>
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">
+            Lastest Transactions
+          </div>
+          <List />
         </div>
       </div>
     </div>
